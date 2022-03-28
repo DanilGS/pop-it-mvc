9c9584ffcba433438f1afd16a8b99daa6a1ce9d3
Гуньков Даниил Сергеевич <gr494_gds@ttit.local>
2022-03-28 13:04:24 +0700

    first commit

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,6 +2,7 @@
 //Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
 
+//Подключение автозагрузчика composer
 require_once __DIR__ . '/../vendor/autoload.php';
 
 //Функция, возвращающая массив всех настроек приложения
@@ -20,6 +21,3 @@ function getConfigs(string $path = DIR_CONFIG): array
 require_once __DIR__ . '/../routes/web.php';
 
 return new Src\Application(new Src\Settings(getConfigs()));
-
-
-
